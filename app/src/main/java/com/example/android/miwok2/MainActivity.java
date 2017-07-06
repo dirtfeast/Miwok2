3e53e461c30f76f3f529831a3460347a6b9de44d
Dan Washburn <dirtfeast@gmail.com>
2017-07-06 15:23:10 -0600

    Add TabLayout

--- app/src/main/java/com/example/android/miwok2/MainActivity.java
+++ app/src/main/java/com/example/android/miwok2/MainActivity.java
@@ -16,6 +16,7 @@
 package com.example.android.miwok2;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
@@ -35,10 +36,21 @@ public class MainActivity extends AppCompatActivity {
 
         // Create an adapter (CategoryAdapter.java)
         // that knows which fragment should be shown on each page
-        CategoryAdapter adapter = new CategoryAdapter(getSupportFragmentManager());
+        CategoryAdapter adapter = new CategoryAdapter(this, getSupportFragmentManager());
 
         // Set the adapter onto the ViewPager
         viewPager.setAdapter(adapter);
+
+        // Find the tab layout that shows the tabs
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+
+        // Connect the tab layout with the view pager. This will
+        //   1. Update the tab layout when the view pager is swiped
+        //   2. Update the view pager when a tab is selected
+        //   3. Set the tab layout's tab names with the view pager's adapter's titles
+        //      by calling onPageTitle()
+        tabLayout.setupWithViewPager(viewPager);
+
     } // Close method onCreate()
 
 } // Close class MainActivity
